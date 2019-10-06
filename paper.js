ded7040f34255cece4125ad94c60fa63c3be1bba
John Teske <john.teske@gmail.com>
2019-10-06 10:58:11 -0700

    Add cut and engrave helpers

--- paper.js
+++ paper.js
@@ -6,8 +6,18 @@ const dpi = 96;
 
 paper.setup(new paper.Size(999, 999));
 
+const cut = o => {
+  o.strokeColor = "#000000";
+  return o;
+};
+const engrave = o => {
+  o.strokeColor = "#0000ff";
+  return o;
+};
+
 //const point = (x, y) => new paper.Point(x, y);
 const drawRect = (...args) => new paper.Path.Rectangle(...args);
+
 const inches = n => n * dpi;
 const mm = n => (n * dpi) / 25.4;
 
@@ -16,7 +26,10 @@ const rect = new paper.Rectangle({
   height: inches(3)
 });
 const r = drawRect(rect, mm(3));
-r.strokeColor = "black";
+cut(r);
+
+const cir = new paper.Path.Circle([inches(1.5), inches(1.5)], mm(3));
+engrave(cir);
 
 const svg = paper.project.exportSVG({ asString: true });
 
