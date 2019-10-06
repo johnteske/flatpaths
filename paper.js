1ba605151941e7e81fb054fea5294bdcd8d8a548
John Teske <john.teske@gmail.com>
2019-10-06 10:41:59 -0700

    Add mm units

--- paper.js
+++ paper.js
@@ -7,14 +7,16 @@ const dpi = 96;
 paper.setup(new paper.Size(999, 999));
 
 //const point = (x, y) => new paper.Point(x, y);
-const rect = o => new paper.Path.Rectangle(o);
+const drawRect = (...args) => new paper.Path.Rectangle(...args);
 const inches = n => n * dpi;
+const mm = n => (n * dpi) / 25.4;
 
-rect({
+const rect = new paper.Rectangle({
   width: inches(3),
-  height: inches(3),
-  strokeColor: "black"
+  height: inches(3)
 });
+const r = drawRect(rect, mm(3));
+r.strokeColor = "black";
 
 const svg = paper.project.exportSVG({ asString: true });
 
