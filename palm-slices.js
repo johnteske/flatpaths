e6bfbead3e7ed2a8f0bb88e6481e495657cfc041
John Teske <john.teske@gmail.com>
2019-10-13 19:59:55 -0700

    Improve boolean logic and display

--- palm-slices.js
+++ palm-slices.js
@@ -12,38 +12,53 @@ paper.setup(new paper.Size(9999, 9999));
 
 const T = inches(1 / 8); // thickness of main body material
 // const T2 = // thickness of faceplate material
-const cornerRadius = 0; // mm(3);
+const cornerRadius = mm(3);
 
-const pin = { d: mm(3) }
+const primitives = new paper.Group({ insert: false });
+
+const pin = { d: mm(3) };
 pin.r = pin.d / 2;
 pin.h = T + pin.d + T; // total pin height, incl. padding
 
 const palm = {
   w: mm(51),
-  h: mm(96),
+  h: mm(96.5),
   d1: mm(7.5), // body
-  d2: mm(8.1) // camera
+  d2: mm(8.1), // body at camera
+  camera: {
+    w: mm(11),
+    h: mm(19),
+    offset: mm(5) // offset both x and y from edge
+  }
 };
 
 const cards = {
   w: mm(54),
   h: mm(86),
   T: mm(1.75) // thickness both cards, stacked = 1.65 mm, with wiggle
-}
+};
 
-const inner = new paper.Rectangle({
+const palmPocket = new paper.Rectangle({
   width: palm.d1,
   height: palm.h,
   x: T,
   y: pin.h
 });
-cut(draw.rect(inner, 0));
+cut(draw.rect({ ...palmPocket, radius: cornerRadius}));
+
+//const palmCameraCutout = new paper.Rectangle({
+//  width: T,
+//  height: palm.camera.h
+//});
+//palmCameraCutout.x = palmPocket.topRight.x;
+//palmCameraCutout.y = palmPocket.topRight.y + palm.camera.offset;
+//cut(draw.rect(palmCameraCutout, 0));
 
 const rect = new paper.Rectangle({
-  width: T + inner.width + T + cards.T,
-  height: pin.h + inner.height + pin.h
+  width: T + palm.d1 + T + cards.T,
+  height: pin.h + palmPocket.height + pin.h
 });
-const rect2 = draw.rect(rect, cornerRadius);
+const rect2 = draw.rect({ ...rect, radius: cornerRadius, parent: primitives });
 
 const cardPocket = new paper.Rectangle({
   width: cards.T,
@@ -51,14 +66,17 @@ const cardPocket = new paper.Rectangle({
   x: rect.width - cards.T,
   y: pin.h
 });
-const cardPocket2 = draw.rect(cardPocket, 0);
+const cardPocket2 = draw.rect({ ...cardPocket, radius: 0, parent: primitives});
 
 const outerWithPocket = rect2.subtract(cardPocket2);
-cut(outerWithPocket)
+outerWithPocket.parent = paper.project.activeLayer;
+cut(outerWithPocket);
 
 const holeAt = center => new paper.Path.Circle({ center, radius: pin.r });
 const holes = new paper.Group(
-  [[rect.width / 2, T + pin.r], [rect.width / 2, rect.height - T - pin.r]].map(xy => holeAt(xy))
+  [[rect.width / 2, T + pin.r], [rect.width / 2, rect.height - T - pin.r]].map(
+    xy => holeAt(xy)
+  )
 );
 cut(holes);
 
