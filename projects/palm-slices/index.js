b16a71046aebd48d2ce438cc09dec4af6d576358
John Teske <john.teske@gmail.com>
2019-10-14 22:23:34 -0700

    Snapshot of working state
    First group is simply the primitives, drawn this time for better
    visual reference.

--- projects/palm-slices/index.js
+++ projects/palm-slices/index.js
@@ -1,7 +1,7 @@
 const paper = require("paper-jsdom");
 
 const path = require("../../path");
-const { cut } = require("../../stroke");
+const { cut, guide } = require("../../stroke");
 const { inches, mm } = require("../../units");
 
 const palm = require("./palm");
@@ -11,9 +11,7 @@ const group = (...args) => new paper.Group(...args);
 
 const T = inches(1 / 8); // thickness of main body material
 // const T2 = // thickness of faceplate material
-const cornerRadius = mm(3);
-
-const primitives = group({ insert: false });
+const cornerRadius = 0; // mm(3);
 
 const pin = { d: mm(3) };
 pin.r = pin.d / 2;
@@ -25,50 +23,107 @@ const palmPocket = new paper.Rectangle({
   x: T,
   y: pin.h
 });
-const cutPalmPocket = () =>
-  cut(path.rect({ ...palmPocket, radius: cornerRadius }));
+const palmPocketGuide = guide(
+  path.rect({ ...palmPocket, radius: cornerRadius })
+);
+const cutPalmPocket = () => cut(palmPocketGuide.clone());
 
-//const palmCameraCutout = new paper.Rectangle({
-//  width: T,
-//  height: palm.camera.h
-//});
-//palmCameraCutout.x = palmPocket.topRight.x;
-//palmCameraCutout.y = palmPocket.topRight.y + palm.camera.offset;
-//cut(path.rect(palmCameraCutout, 0));
+const palmFace = new paper.Rectangle({
+  width: T,
+  height: palm.face.h,
+  y: palmPocket.y + palm.face.y // TODO
+});
+const cutPalmFace = () => cut(path.rect({ ...palmFace }));
+const palmCameraCutout = new paper.Rectangle({
+  width: T,
+  height: palm.camera.h,
+  x: palmPocket.topRight.x,
+  y: palmPocket.topRight.y + palm.camera.offset
+});
+const cameraCutoutGuide = guide(path.rect(palmCameraCutout, 0));
 
 const rect = new paper.Rectangle({
   width: T + palm.d1 + T + cards.T,
   height: pin.h + palmPocket.height + pin.h
 });
-const rect2 = path.rect({ ...rect, radius: cornerRadius, parent: primitives });
+const rect2 = path.rect({ ...rect, radius: cornerRadius });
 
 const cardPocket = new paper.Rectangle({
   width: cards.T,
   height: cards.h,
   x: rect.width - cards.T,
-  y: (rect.height - cards.h) / 2
+  y: pin.h // (rect.height - cards.h) / 2
 });
-const cardPocket2 = path.rect({ ...cardPocket, radius: 0, parent: primitives });
+const cardPocket2 = path.rect({ ...cardPocket, radius: 0 });
+
+const outer = () => {
+  const o = rect2.clone();
+  //o.parent = paper.project.activeLayer;
+  return cut(o);
+};
 
 const outerWithPocket = () => {
-  const owp = rect2.subtract(cardPocket2);
-  owp.parent = paper.project.activeLayer;
+  const owp = rect2.clone().subtract(cardPocket2);
+  //owp.parent = paper.project.activeLayer;
   return cut(owp);
 };
 
+const outerWithPocketAndFace = outerWithPocket().subtract(
+  cutPalmPocket().unite(cutPalmFace())
+);
+
 const holeAt = center => new paper.Path.Circle({ center, radius: pin.r });
-const holes = () =>
-  cut(
-    group(
-      [
-        [rect.width / 2, T + pin.r],
-        [rect.width / 2, rect.height - T - pin.r]
-      ].map(xy => holeAt(xy))
-    )
-  );
-
+const holes = guide(
+  group(
+    [
+      [rect.width / 2, T + pin.r],
+      [rect.width / 2, rect.height - T - pin.r]
+    ].map(xy => holeAt(xy))
+  )
+);
+const cutHoles = () => cut(holes.clone());
 // left edge
-group([outerWithPocket(), holes()]);
+group([outer(), cutHoles()]).translate([100, 0]);
 
-// middle
-group([outerWithPocket(), cutPalmPocket(), holes()]).translate([200, 0]);
+// left inner
+group([outerWithPocket(), cutPalmPocket(), cutHoles()]).translate([200, 0]);
+
+// phone face
+group([outerWithPocketAndFace.clone(), cutHoles()]).translate([300, 0]);
+
+// phone middle (charge port access)
+// TODO this will also remove the bottom face hooks
+group([
+  outerWithPocketAndFace.clone().subtract(
+    cut(
+      path.rect(
+        new paper.Rectangle({
+          x: 0,
+          y: rect.height,
+          width: T + palm.d1,
+          height: -rect.height / 2
+        })
+      )
+    )
+  ).subtract(
+    cut(
+      path.rect(
+        new paper.Rectangle({
+          x: 0,
+          y: rect.height,
+          width: rect.width,
+          height: -pin.h
+        })
+      )
+    )
+   ),
+
+  cutHoles()
+]).translate([400, 0]);
+
+// with camera
+group([
+  outerWithPocketAndFace.clone(),
+  cut(cameraCutoutGuide.clone()),
+  cutHoles()
+]).translate([500, 0]);
--- projects/palm-slices/palm.js
+++ projects/palm-slices/palm.js
@@ -1,6 +1,6 @@
 const { mm } = require("../../units");
 
-module.exports = {
+const palm = {
   w: mm(51),
   h: mm(96.5),
   d1: mm(7.5), // body
@@ -9,5 +9,18 @@ module.exports = {
     w: mm(11),
     h: mm(19),
     offset: mm(5) // offset both x and y from edge
+  },
+  button: {
+    y: mm(18.5),
+    h: mm(9.75),
+    w: mm(2.25)
   }
 };
+
+palm.face = {
+  y: mm(10.5), // from top
+  y2: mm(12) // from bottom
+};
+palm.face.h = palm.h - palm.face.y - palm.face.y2;
+
+module.exports = palm;
