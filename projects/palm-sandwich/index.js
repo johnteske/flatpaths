1afba621e9843632b0b44ed07d39c6d3aa43dea3
John Teske <john.teske@gmail.com>
2019-11-05 22:52:04 -0800

    Pin corners by cardOuter geometry

--- projects/palm-sandwich/index.js
+++ projects/palm-sandwich/index.js
@@ -5,7 +5,7 @@ const paper = require("paper-jsdom");
 const path = require(`${root}/path`);
 //const withRoundedCorner = require("../../rounded-corner");
 const { cut, guide } = require(`${root}/stroke`);
-const { inches, mm } = require(`${root}/units`);
+const { inches } = require(`${root}/units`);
 
 const group = (...args) => new paper.Group(...args);
 //const pipe = (...fns) => x => fns.reduce((v, f) => f(v), x);
@@ -16,24 +16,36 @@ const cards = require(`${root}/objects/cards`);
 const cardCutout = require("./cardCutout");
 const pin = require("./pin");
 
-const pins = group([[37, 66], [12, 25]].map(point => pin().translate(point)));
-
 const T = inches(1 / 8); // thickness of main body material
 const outerWidth = T * 2;
 
+const cardOuter = new paper.Rectangle({
+  width: outerWidth + cards.w + outerWidth,
+  height: outerWidth + cards.h + outerWidth
+});
+
 // TODO split in halves to save material
-const cardOuter = path
+const cardOuterPath = path
   .rect({
-    width: outerWidth + cards.w + outerWidth,
-    height: outerWidth + cards.h + outerWidth,
-    radius: outerWidth
+    ...cardOuter,
+
+    radius: T
   })
   .subtract(cardCutout().translate([outerWidth, outerWidth]));
+
+const pins = group(
+  [
+    [outerWidth / 2, outerWidth / 2],
+    [cardOuter.width - outerWidth / 2, outerWidth / 2],
+    [outerWidth / 2, cardOuter.height - outerWidth / 2],
+    [cardOuter.width - outerWidth / 2, cardOuter.height - outerWidth / 2]
+  ].map(point => pin().translate(point))
+);
 
 const guides = [cardCutout().translate([outerWidth, outerWidth]), pins.clone()];
 guides.forEach((g, i) => guide(g).translate([i * cards.w, cards.h + 66]));
 
-const cuts = [group(cardOuter, pins.clone())];
+const cuts = [group(cardOuterPath, pins.clone())];
 cuts.forEach((g, i) => cut(g).translate([i * cards.w, 0]));
 
 //const group = (...args) => new paper.Group(...args);
