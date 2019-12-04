babf2c7204387d2008a8ad06731f2a7c589b62aa
John Teske <john.teske@gmail.com>
2019-12-04 07:45:22 -0800

    Translate palm cutout for guide placement

--- projects/palm-sandwich/constructs/palm-cutout.js
+++ projects/palm-sandwich/constructs/palm-cutout.js
@@ -5,11 +5,15 @@ const { mm } = require(`${root}/units`);
 
 const palm = require(`${root}/objects/palm`);
 
+const { width } = require("../constructs/frame");
+const { cardOuterGeometry } = require("../constructs/card-outer");
+
 const cutout = path.rect({
   width: palm.w,
   height: palm.h,
   radius: mm(9) // TODO palm object does not have radius // palm.r
 });
+cutout.translate([(cardOuterGeometry.width - palm.w) / 2, width]);
 
 module.exports = () => {
   return cutout.clone();
