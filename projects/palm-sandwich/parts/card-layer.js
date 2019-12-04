cf4c59fa1060959bed2014af56ea15f943743b9c
John Teske <john.teske@gmail.com>
2019-12-04 07:41:19 -0800

    Center card cutout vertically

--- projects/palm-sandwich/parts/card-layer.js
+++ projects/palm-sandwich/parts/card-layer.js
@@ -5,11 +5,10 @@ const { pipe } = require(`${root}/fn`);
 
 const { cardOuter, pins } = require("../constructs/card-outer");
 const cardCutout = require("../constructs/card-cutout");
-const { width } = require("../constructs/frame");
 
 const cardOuterPart = pipe(
   ...pins().map(subtract),
-  subtract(cardCutout().translate(width))
+  subtract(cardCutout())
 )(cardOuter());
 
 module.exports = () => {
