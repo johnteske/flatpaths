3ddbf091e6723666ae68f8e5e26c47b28b069aa7
John Teske <john.teske@gmail.com>
2019-12-10 20:48:38 -0800

    Adjust frame width

--- projects/palm-sandwich/constructs/frame.js
+++ projects/palm-sandwich/constructs/frame.js
@@ -1,8 +1,7 @@
 const T = require("../material");
 const { pinGeometry } = require("./pin");
 
-// const width = Math.max(Math.max(T * 2, pinGeometry.d + T), pinGeometry.head.d);
-const width = pinGeometry.head.r + pinGeometry.r;
+const width = Math.max(T * 2, pinGeometry.head.r + pinGeometry.r);
 
 module.exports = {
   width
