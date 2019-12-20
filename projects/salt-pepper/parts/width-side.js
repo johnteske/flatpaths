ed24b6e7e73709f6f83c06fb00d5026bea7e9fdd
John Teske <john.teske@gmail.com>
2019-12-19 21:18:52 -0800

    Finish width-length joint

--- projects/salt-pepper/parts/width-side.js
+++ projects/salt-pepper/parts/width-side.js
@@ -1,12 +1,18 @@
 const root = require("app-root-path");
 
+const { inches } = require(`${root}/units`);
 const { unite } = require(`${root}/boolean`);
 const { pipe } = require(`${root}/fn`);
 
 const { widthSide } = require("../constructs/panels");
-
 const { widthLengthFingers } = require("../constructs/joints");
+const { T } = require("../material");
 
-const widthSidePart = pipe(...widthLengthFingers().map(unite))(widthSide());
+const widthSidePart = pipe(
+  ...widthLengthFingers().map(unite),
+  ...widthLengthFingers()
+    .map(f => f.translate([inches(2.5) + T, 0]))
+    .map(unite)
+)(widthSide());
 
 module.exports = () => widthSidePart.clone();
