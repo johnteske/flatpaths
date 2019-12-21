55a5762853a6b201e11aff678a41c91007205b1f
John Teske <john.teske@gmail.com>
2019-12-21 15:44:58 -0800

    Add Pro Micro standoff for keyboard builds

---
+++ projects/pro-micro-standoff/index.js
@@ -0,0 +1,44 @@
+const root = require("app-root-path");
+
+const { layoutRowsWithOffset } = require(`${root}/distribution`);
+const group = require(`${root}/group`);
+const { subtract } = require(`${root}/boolean`);
+const { pipe } = require(`${root}/fn`);
+const { cut, guide } = require(`${root}/stroke`);
+const { inches } = require(`${root}/units`);
+const path = require(`${root}/path`);
+
+const pitch = inches(0.1);
+const pinHoleDiameter = inches(0.04);
+
+const rowDistance = 100; // TODO
+
+const pinHole = path.circle({
+  radius: pinHoleDiameter / 2
+});
+
+const row = () =>
+  Array.from({ length: 12 }).map((v, i) =>
+    pinHole.clone().translate([i * pitch, 0])
+  );
+
+const bottomRow = () => row().map(p => p.translate([0, rowDistance]));
+
+const support = () =>
+  path.rect({
+    width: 11 * pitch,
+    height: 100
+  });
+
+layoutRowsWithOffset(
+  [
+    [group(support(), ...row(), ...bottomRow())].map(guide),
+    [
+      pipe(
+        ...row().map(subtract),
+        ...bottomRow().map(subtract)
+      )(support())
+    ].map(cut)
+  ],
+  10
+);
