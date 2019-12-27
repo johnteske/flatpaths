0bcf842915bc0da9093cc732827e59f93a319a06
John Teske <john.teske@gmail.com>
2019-12-26 21:24:15 -0800

    Clean up for linting

---
+++ constructs/cantilever-snap-fit.js
@@ -0,0 +1,25 @@
+/*
+Based on:
+https://lasercutlikeaboss.weebly.com/uploads/2/7/8/8/27883957/advancedjoinery_master_web.pdf
+*/
+
+//const root = require("app-root-path");
+
+//const { mm } = require(`${root}/units`);
+
+//const snap = ({
+//  t = mm(3), // thickness of material
+//  l1 = mm(3), // height of gutter 1 (strength..flexibility in pushing towards center)
+//  // l2 = mm(3), // length of the nose face (calculated)
+//  l3 = mm(3), // height of gutter 2 (strength..flexibility in pulling towards center)
+//  l4 = mm(6), // useale length of finger, must be longer than `t`,
+//  w1 = mm(3), // width of finger, must be less than w2
+//  w2 = mm(3), // overhang width, use w1 + w4 as default, must not be greater than w1 + w4
+//  w3 = mm(12), // slot width, between the insides of fingers
+//  w4 = mm(3), // width of inside gutter (strength..flexibility), limits deformation towards center
+//  w5 = mm(3), // width of outside gutter (strength..flexibility), limits deformation away from center
+//  slipAngle = 45, // angle of the nose leading edge (..ease)
+//  returnAngle = 45 // angle of the nose underside edge (permanance..reversible/slop)
+//}) => {};
+
+//const receiving
