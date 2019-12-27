82e84471ae2ee6cdbdd4e1a8ddb4d06c59c20ea3
John Teske <john.teske@gmail.com>
2019-12-27 13:51:59 -0800

    Add shelf part and adjust dimensions
    During the design, using a smaller and more rectangular shape
    should help visualize the changes to be made.

--- projects/parts-box/dimensions.js
+++ projects/parts-box/dimensions.js
@@ -3,10 +3,15 @@ const root = require("app-root-path");
 const { inches } = require(`${root}/units`);
 
 // IKEA Kallax interior shelf dimensions = 13.25" square, 15" deep
-const size = inches(13);
+// const kallax = inches(13);
+
+// hall shelf
+const width = inches(8);
+const height = inches(10);
+const depth = inches(9);
 
 module.exports = {
-  width: size,
-  height: size,
-  depth: size
+  width,
+  height,
+  depth
 };
