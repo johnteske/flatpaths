7587741bc71016c01399f82651e49b58c145bcf0
John Teske <john.teske@gmail.com>
2019-12-27 13:04:17 -0800

    Prettier

--- generate.js
+++ generate.js
@@ -1,11 +1,11 @@
-const argv = require('minimist')(process.argv.slice(2));
+const argv = require("minimist")(process.argv.slice(2));
 const paper = require("paper-jsdom");
 const path = require("path");
 const fs = require("fs");
 
 paper.setup(new paper.Size(999, 999));
 
-require(`./projects/${argv.p}`)
+require(`./projects/${argv.p}`);
 
 const svg = paper.project.exportSVG({ asString: true });
 
