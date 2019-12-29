b80a56c35da1c41e40933928ac627bb7d8ebe674
John Teske <john.teske@gmail.com>
2019-12-29 12:52:07 -0800

    Add height joints

--- projects/parts-box/constructs/drawer.js
+++ projects/parts-box/constructs/drawer.js
@@ -1,10 +1,12 @@
 const dimensions = require("../dimensions");
-const { NUM_DRAWERS } = dimensions;
+const { NUM_DRAWERS, NUM_SHELVES } = dimensions;
 
 const { T } = require("../material");
 
 const width = (dimensions.width - T * (NUM_DRAWERS + 1)) / NUM_DRAWERS;
+const height = (dimensions.height - T * (NUM_SHELVES + 1)) / NUM_SHELVES;
 
 module.exports = {
-  width
+  width,
+  height
 };
--- projects/parts-box/parts/body/back.js
+++ projects/parts-box/parts/body/back.js
@@ -3,6 +3,7 @@ const root = require("app-root-path");
 const { pipe } = require(`${root}/fn`);
 const { subtract, unite } = require(`${root}/boolean`);
 const path = require(`${root}/path`);
+const { guide } = require(`${root}/stroke`);
 
 const fingerJoint = require(`${root}/constructs/finger-joint`);
 
@@ -10,7 +11,10 @@ const drawer = require("../../constructs/drawer");
 const dimensions = require("../../dimensions");
 const { T } = require("../../material");
 
-const { NUM_DRAWERS } = dimensions;
+const { NUM_DRAWERS, NUM_SHELVES } = dimensions;
+
+const rotate = (...args) => target => target.rotate(...args);
+const translate = (...args) => target => target.translate(...args);
 
 const panel = path.rect({
   width: dimensions.width,
@@ -23,15 +27,35 @@ const widthJointSection = fingerJoint({
   n: 5
 });
 
-const translate = (...args) => target => target.translate(args);
+const widthJoint = () =>
+  Array.from({ length: NUM_DRAWERS }).reduce(
+    (acc, _, i) =>
+      acc.concat(
+        widthJointSection().map(translate(T + i * (drawer.width + T), 0))
+      ),
+    []
+  );
 
-const widthJoint = () => Array.from({ length: NUM_DRAWERS }).reduce(
-  (acc, _, i) =>
-    acc.concat(
-      widthJointSection().map(translate(T + i * (drawer.width + T), 0))
-    ),
-  []
-);
+const heightJointSection = fingerJoint({
+  width: drawer.height,
+  height: T,
+  n: 5
+});
+
+const heightJoint = () =>
+  Array.from({ length: NUM_SHELVES })
+    .reduce(
+      (acc, _, i) =>
+        acc.concat(
+          heightJointSection().map(translate(T + i * (drawer.height + T), 0))
+        ),
+      []
+    )
+    .map(rotate(90, [0, 0]));
+
+//heightJoint()
+//  .map(translate(T, 0))
+//  .map(guide)
 
 // dimensions are external--
 // --finger joints will be inside these dimensions
@@ -44,8 +68,7 @@ const back = () =>
         height: T
       })
     ),
-    ...widthJoint()
-      .map(unite),
+    ...widthJoint().map(unite),
     // bottom
     subtract(
       path.rect({
@@ -56,7 +79,28 @@ const back = () =>
     ),
     ...widthJoint()
       .map(translate(0, dimensions.height - T))
+      .map(unite),
+    // left
+    subtract(
+      path.rect({
+        width: T,
+        height: dimensions.height
+      })
+    ),
+    ...heightJoint()
+      .map(translate(T, 0))
+      .map(unite),
+    // right
+    subtract(
+      path.rect({
+        x: dimensions.width - T,
+        width: T,
+        height: dimensions.height
+      })
+    ),
+    ...heightJoint()
+      .map(translate(dimensions.width, 0))
       .map(unite)
-   )(panel);
+  )(panel);
 
 module.exports = back;
