2df60d147d1c43d0512d0dd86836651ffe660f3d
John Teske <john.teske@gmail.com>
2019-12-28 21:29:53 -0800

    Add initial finger joints

--- projects/parts-box/parts/body/back.js
+++ projects/parts-box/parts/body/back.js
@@ -1,15 +1,36 @@
 const root = require("app-root-path");
 
+const { pipe } = require(`${root}/fn`);
+const { subtract, unite } = require(`${root}/boolean`);
 const path = require(`${root}/path`);
 
+const fingerJoint = require(`${root}/constructs/finger-joint`);
+
 const dimensions = require("../../dimensions");
+const { T } = require("../../material");
+
+const panel = path.rect({
+  width: dimensions.width,
+  height: dimensions.height
+});
 
 // dimensions are external--
 // --finger joints will be inside these dimensions
 const back = () =>
-  path.rect({
-    width: dimensions.width,
-    height: dimensions.height
-  });
+  pipe(
+    subtract(
+      // remove top joint material
+      path.rect({
+        width: dimensions.width,
+        height: T
+      })
+    ),
+
+    ...fingerJoint({
+      width: dimensions.width,
+      height: T,
+      n: 7
+    })().map(unite)
+  )(panel);
 
 module.exports = back;
