cfb0c25ea5c4f633634d69c7b6d78385a62591ca
John Teske <john.teske@gmail.com>
2019-12-28 21:29:53 -0800

    Add initial finger joints

--- projects/parts-box/dimensions.js
+++ projects/parts-box/dimensions.js
@@ -6,9 +6,13 @@ const { inches } = require(`${root}/units`);
 // const kallax = inches(13);
 
 // hall shelf
-const width = inches(8);
-const height = inches(10);
-const depth = inches(9);
+//const width = inches(8);
+//const height = inches(10);
+//const depth = inches(9);
+
+const width = inches(5);
+const height = inches(4);
+const depth = inches(3);
 
 module.exports = {
   width,
