8a57cdf387fc14b7891dbbc3d02c76312598bfa6
John Teske <john.teske@gmail.com>
2019-12-30 15:49:43 -0800

    Apply joint to shelf as "A" part
    TODO convert this to the "B" part.

--- projects/parts-box/parts/body/shelf.js
+++ projects/parts-box/parts/body/shelf.js
@@ -1,15 +1,26 @@
+// TODO this needs to be the "B" part
+//
 const root = require("app-root-path");
+
+const { pipe } = require(`${root}/fn`);
 
 const path = require(`${root}/path`);
 
+const { applyFingerJoint } = require(`${root}/constructs/finger-joint2`);
+
 const dimensions = require("../../dimensions");
 
-// dimensions are external--
-// --finger joints will be inside these dimensions
-const shelf = () =>
+const shelfBackJoint = require("../../constructs/shelf-back-joint");
+
+//
+
+const panel = () =>
   path.rect({
     width: dimensions.width,
     height: dimensions.depth
   });
+
+const shelf = () =>
+  pipe(...shelfBackJoint.joint().flatMap(applyFingerJoint))(panel());
 
 module.exports = shelf;
