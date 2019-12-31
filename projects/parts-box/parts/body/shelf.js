e5d101dff0b965a3159d17fc2dec82aa7493ee1d
John Teske <john.teske@gmail.com>
2019-12-30 21:28:24 -0800

    Use part "b" for shelf joint

--- projects/parts-box/parts/body/shelf.js
+++ projects/parts-box/parts/body/shelf.js
@@ -1,26 +1,31 @@
-// TODO this needs to be the "B" part
-//
 const root = require("app-root-path");
 
 const { pipe } = require(`${root}/fn`);
 
 const path = require(`${root}/path`);
+const { subtract } = require(`${root}/boolean`);
 
 const { applyFingerJoint } = require(`${root}/constructs/finger-joint2`);
 
 const dimensions = require("../../dimensions");
+const { T } = require("../../material");
 
 const shelfBackJoint = require("../../constructs/shelf-back-joint");
 
 //
 
+const { width } = dimensions;
+
 const panel = () =>
   path.rect({
-    width: dimensions.width,
+    width,
     height: dimensions.depth
   });
 
 const shelf = () =>
-  pipe(...shelfBackJoint.joint().flatMap(applyFingerJoint))(panel());
+  pipe(
+    subtract(path.rect({ width, height: T })),
+    ...shelfBackJoint.joint("b").flatMap(applyFingerJoint)
+  )(panel());
 
 module.exports = shelf;
