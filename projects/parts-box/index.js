a53b014cebda34f3b51edd3f92183938557a2463
John Teske <john.teske@gmail.com>
2019-12-31 13:02:24 -0800

    Add shelf slots for dividers

--- projects/parts-box/index.js
+++ projects/parts-box/index.js
@@ -11,11 +11,11 @@ const root = require("app-root-path");
 const paper = require("paper-jsdom");
 
 const { layoutRowsWithOffset } = require(`${root}/distribution`);
-const { guide } = require(`${root}/stroke`);
+const { cut } = require(`${root}/stroke`);
 
 const { T } = require("./material");
 const body = require("./parts/body");
 
-layoutRowsWithOffset([body().map(guide)], T);
+layoutRowsWithOffset([body().map(cut)], T);
 
 paper.view.viewSize = [9999, 9999];
--- projects/parts-box/parts/body/shelf.js
+++ projects/parts-box/parts/body/shelf.js
@@ -11,6 +11,7 @@ const { applyFingerJoint } = require(`${root}/constructs/finger-joint2`);
 const dimensions = require("../../dimensions");
 const { T } = require("../../material");
 
+const drawer = require("../../constructs/drawer");
 const shelfBackJoint = require("../../constructs/shelf-back-joint");
 const shelfSideJoint = require("../../constructs/shelf-side-joint");
 
@@ -26,21 +27,32 @@ const panel = () =>
 
 const shelf = () =>
   pipe(
-    // back joint
-    subtract(path.rect({ width, height: T })),
-    ...shelfBackJoint.joint("b").flatMap(applyFingerJoint),
     // left joint
     ...pipe(
       rotate(90, [0, 0]),
       translateX(T),
       applyFingerJoint
     )(shelfSideJoint.joint("b")),
+    // interior joints, for shelf dividers
+    ...pipe(
+      rotate(90, [0, 0]),
+      drawer.translateByWidths(1),
+      translateX(T),
+      applyFingerJoint
+    )(shelfSideJoint.joint("a", 0)),
+    ///    ...shelfSideJoint.interiorJoints()
+    //      .flatMap(rotate(90, [0, 0]))
+    //      //.flatMap(translateX(width))
+    //      .flatMap(applyFingerJoint),
     // right joint
     ...pipe(
       rotate(90, [0, 0]),
       translateX(width),
       applyFingerJoint
-    )(shelfSideJoint.joint("b"))
+    )(shelfSideJoint.joint("b")),
+    // back joint
+    subtract(path.rect({ width, height: T })),
+    ...shelfBackJoint.joint("b").flatMap(applyFingerJoint)
   )(panel());
 
 module.exports = shelf;
