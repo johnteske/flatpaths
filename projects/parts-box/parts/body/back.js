9a4a598a677ffdbaf0f544bdb4bebc8d7163975c
John Teske <john.teske@gmail.com>
2019-12-30 22:12:12 -0800

    Add shelf-side joint

--- projects/parts-box/parts/body/back.js
+++ projects/parts-box/parts/body/back.js
@@ -1,25 +1,15 @@
 const root = require("app-root-path");
 
-const { pipe, nItems } = require(`${root}/fn`);
-const { mm } = require(`${root}/units`);
+const { pipe } = require(`${root}/fn`);
 const path = require(`${root}/path`);
-const {
-  rotate,
-  translateX,
-  translateY,
-  flipH,
-  flipV
-} = require(`${root}/transform`);
+const { flipH, flipV } = require(`${root}/transform`);
 
-const {
-  fingerJoint,
-  applyFingerJoint
-} = require(`${root}/constructs/finger-joint2`);
+const { applyFingerJoint } = require(`${root}/constructs/finger-joint2`);
 
 const drawer = require("../../constructs/drawer");
 const shelfBackJoint = require("../../constructs/shelf-back-joint");
+const shelfSideJoint = require("../../constructs/shelf-side-joint");
 const dimensions = require("../../dimensions");
-const { T } = require("../../material");
 
 const { NUM_DRAWERS, NUM_SHELVES } = dimensions;
 
@@ -32,32 +22,6 @@ const panel = path.rect({
   height
 });
 
-const translateByDrawerWidth = i => translateX(i * (drawer.width + T));
-const translateByDrawerHeight = i => translateY(i * (drawer.height + T));
-
-const heightJointSection = radius =>
-  fingerJoint({
-    width: drawer.height,
-    height: T,
-    n: 5,
-    radius
-  }).a();
-
-const heightJoint = r =>
-  nItems(NUM_SHELVES)
-    .flatMap((_, i) =>
-      heightJointSection(r).translate(T + i * (drawer.height + T), 0)
-    )
-    .map(rotate(90, [0, 0]))
-    .map(translateX(T));
-
-const heightJoints = () =>
-  nItems(NUM_DRAWERS - 1).flatMap((_, i) =>
-    heightJoint(0)
-      .map(translateByDrawerWidth(i + 1))
-      .map(translateX(T))
-  );
-
 const back = () =>
   pipe(
     // top
@@ -67,18 +31,20 @@ const back = () =>
     // bottom
     ...shelfBackJoint
       .joint("a")
-      .map(translateByDrawerHeight(NUM_SHELVES))
+      .map(drawer.translateByHeights(NUM_SHELVES))
       .map(flipV)
       .flatMap(applyFingerJoint),
     // left
-    ...heightJoint(mm(0.5))
+    ...shelfSideJoint
+      .joint("a")
       .map(flipH)
       .flatMap(applyFingerJoint),
     // middle
-    ...heightJoints().flatMap(applyFingerJoint),
+    ...shelfSideJoint.interiorJoints("a").flatMap(applyFingerJoint),
     // right
-    ...heightJoint(mm(0.5))
-      .map(translateByDrawerWidth(NUM_DRAWERS))
+    ...shelfSideJoint
+      .joint("a")
+      .map(drawer.translateByWidths(NUM_DRAWERS))
       .flatMap(applyFingerJoint)
   )(panel);
 
