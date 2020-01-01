8685dfd333742864fcd49b8a83ab32567788cbcf
John Teske <john.teske@gmail.com>
2020-01-01 10:38:04 -0800

    Output execution time

--- generate.js
+++ generate.js
@@ -3,6 +3,8 @@ const paper = require("paper-jsdom");
 const path = require("path");
 const fs = require("fs");
 
+const startTime = process.hrtime();
+
 paper.setup(new paper.Size(999, 999));
 
 require(`./projects/${argv.p}`);
@@ -11,5 +13,9 @@ const svg = paper.project.exportSVG({ asString: true });
 
 fs.writeFile(path.resolve("./out.svg"), svg, function(err) {
   if (err) throw err;
+
+  const endTime = process.hrtime(startTime);
+  console.info("Execution time: %dms", Math.round(endTime[1] / 100000));
+
   console.log("Saved!");
 });
