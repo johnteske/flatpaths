8d2bf11546d01349cce7f5fba9af2477a7361dd0
John Teske <john.teske@gmail.com>
2020-01-01 10:45:49 -0800

    Add file size log after generation

--- generate.js
+++ generate.js
@@ -14,8 +14,15 @@ const svg = paper.project.exportSVG({ asString: true });
 fs.writeFile(path.resolve("./out.svg"), svg, function(err) {
   if (err) throw err;
 
+  console.log("Saved!");
+
   const endTime = process.hrtime(startTime);
   console.info("Execution time: %dms", Math.round(endTime[1] / 100000));
-
-  console.log("Saved!");
+  console.info("File size: %dkB", getFilesizeInKilobytes(path.resolve("./out.svg")))
 });
+
+function getFilesizeInKilobytes(filename) {
+  var stats = fs.statSync(filename)
+  var fileSizeInBytes = stats["size"]
+  return fileSizeInBytes / 1000
+}
