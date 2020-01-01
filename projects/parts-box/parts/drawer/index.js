a1e7428e02bee0ae6338753953dfa381bf3aa6be
John Teske <john.teske@gmail.com>
2019-12-31 16:26:14 -0800

    Align the width side of the drawer to check both joints

--- projects/parts-box/parts/drawer/index.js
+++ projects/parts-box/parts/drawer/index.js
@@ -1,6 +1,6 @@
 const bottom = require("./bottom");
 const depthSide = require("./depth-side");
-const widthSide = require("./width-side");
+//const widthSide = require("./width-side");
 //const divider = require("./divider");
 
-module.exports = () => [bottom(), depthSide(), widthSide()];
+module.exports = () => [bottom(), depthSide() /*, widthSide()*/];
