ae5dc3eea24362ce85537817acd937757b385b21
John Teske <john.teske@gmail.com>
2020-01-02 21:00:19 -0800

    Map over multiple glyphs to test x translation

--- projects/marks/index.js
+++ projects/marks/index.js
@@ -2,26 +2,29 @@ const paper = require("paper-jsdom");
 
 const cardinalToPoint = require("./cardinalToPoint");
 
-const glyphs = {
-  a: "swnn nnse"
+paper.project.currentStyle = {
+  strokeColor: "#000000"
 };
 
-const cardinalDyadToPoints = dyad => {
+const glyphs = {
+  a: "swnn nnse",
+  b: "nwww wwee eesw"
+};
+
+const cardinalDyadToPointDyad = dyad => {
   const a = dyad.substr(0, 2);
   const b = dyad.substr(2);
   return [cardinalToPoint[a], cardinalToPoint[b]];
 };
 
-const drawGlyph = glyphData => {
-  const pathData = glyphData
+const glyphToPath = (glyphData, i = 0) =>
+  glyphData
     .split(" ")
-    .map(cardinalDyadToPoints)
+    .map(cardinalDyadToPointDyad)
+    .map(([p1, p2]) => [[p1[0] + i, p1[1]], [p2[0] + i, p2[1]]]) // transpose by character
     .map(v => `M${v[0]} L${v[1]}`)
     .join(" ");
 
-  return new paper.CompoundPath(pathData);
-};
-
-const test = drawGlyph(glyphs.a);
-test.scale(48, 48, [0, 0]);
-test.strokeColor = "#000000";
+[glyphs.a, glyphs.b].map((glyph, i) =>
+  new paper.CompoundPath(glyphToPath(glyph, i)).scale(48, 48, [0, 0])
+);
