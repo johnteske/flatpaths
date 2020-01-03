cd31639335da398ba1555a76b09787449a6d6e27
John Teske <john.teske@gmail.com>
2020-01-02 22:58:12 -0800

    Add drafts on all alphanumeric glyphs (insensitve)

--- projects/marks/glyphs.js
+++ projects/marks/glyphs.js
@@ -3,18 +3,44 @@ const glyphs = {
   b: "nwww eesw",
   c: "neww wwse",
   d: "neee wwse",
-  e: "nwne eeww swse",
-  f: "nenw eeww wwsw",
+  e: "nwne wwee swse",
+  f: "nenw wwee wwsw",
   g: "neww wwse seee",
-  h: "nwsw wwse",
-  i: "nwsw",
-  j: "nese seww",
+  h: "nwsw wwee",
+  i: "swnw",
+  j: "nese sesw",
+
   k: "nwsw neww seww",
   l: "nwsw swse",
+  m: "swnw nwee eene",
+  n: "swnw nwse",
+  o: "nwne swse swww neee",
+  p: "wwsw nwee",
+  q: "nwne swww neee nwse",
+  r: "swww wwee",
+  s: "nenw nwee eesw",
+  t: "wwee nese",
 
+  u: "wwsw seee swse",
+  v: "nwsw swne",
+  w: "nwsw swne neee",
+  x: "nwse swne",
+  y: "nwee eese",
+  z: "nwne nesw swse",
+
+  1: "wwne nese",
+  2: "wwne neee eesw swse",
+  3: "nwne neww wwse sesw",
+  4: "neww eese",
+  5: "nenw nwww wwee eesw",
   6: "neww wwse seee eeww",
+  7: "wwee eesw", // full height
+  //7: "nwne nesw", // half height
+  8: "nwne nwse swne swse",
+  9: "eenw nwne nesw",
+  0: "nwne swse nwsw nese nesw",
 
-  NOT_FOUND: "nwne swse nwsw nese"
+  NOT_FOUND: "nwne swse nwsw nese nwse nesw"
 };
 
 const getGlyph = c => glyphs[c] || glyphs.NOT_FOUND;
