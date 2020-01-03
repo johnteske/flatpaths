e32a6072e5bcc63f11ac90aac10568ce1b48f97f
John Teske <john.teske@gmail.com>
2020-01-02 19:42:16 -0800

    Add initial test for system to mark parts
    Add glyphs with:
    - mostly straight lines
    - minimal strokes
    - minimal stroke length

---
+++ projects/marks/cardinalToPoint.js
@@ -0,0 +1,23 @@
+// y
+const N = 0;
+const y = 0.5;
+const S = 1;
+
+// x
+const W = 0;
+const x = 0.5;
+const E = 1;
+
+const cardinalToPoint = {
+  nw: [W, N],
+  nn: [x, N],
+  ne: [E, N],
+  ww: [W, y],
+  cc: [x, y],
+  ee: [E, y],
+  sw: [W, S],
+  ss: [x, S],
+  se: [E, S]
+};
+
+module.exports = cardinalToPoint;
