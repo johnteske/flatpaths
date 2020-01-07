a59bc73c95ba8751dfa491b3e5140ef7379f73b1
John Teske <john.teske@gmail.com>
2020-01-06 20:43:28 -0800

    Log frame width choice

--- projects/palm-sandwich/constructs/frame.js
+++ projects/palm-sandwich/constructs/frame.js
@@ -1,7 +1,23 @@
 const T = require("../material");
 const { pinGeometry } = require("./pin");
 
-const width = Math.max(T * 2, pinGeometry.head.r + pinGeometry.r);
+const a = {
+  label: "T * 2",
+  value: T * 2
+};
+
+const b = {
+  label: "pin head radius + pin radius",
+  value: pinGeometry.head.r + pinGeometry.r
+};
+
+const width = Math.max(a.value, b.value);
+
+console.log(
+  `Frame width ~${width.toFixed(2)} (based on ${
+    width === a.value ? a.label : b.label
+  })`
+);
 
 module.exports = {
   width
