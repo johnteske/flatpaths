a5e747680e0090efc829159c6ccb02455465767a
John Teske <john@98point6.com>
2020-01-10 13:43:55 -0800

    Add alternate guide for snap receiver

--- projects/palm-sandwich/constructs/snap-receiver.js
+++ projects/palm-sandwich/constructs/snap-receiver.js
@@ -2,28 +2,52 @@ const root = require("app-root-path");
 
 const path = require(`${root}/path`);
 const { mm } = require(`${root}/units`);
+const { translate } = require(`${root}/transform`);
 const usb = require("./usb-port-cutout");
 const { cardOuterGeometry } = require("./card-outer");
-const geometry = {
-  width: (cardOuterGeometry.width - usb.geometry.width) / 2,
-  height: 100
-};
 
-const construct = path.rect({
+const geometry = {
+  width: (cardOuterGeometry.width - usb.geometry.width) / 2
+};
+geometry.height = geometry.width;
+
+const cover = path.rect({
   ...geometry,
   radius: mm(0.5)
 });
 
-const constructTranslated = () =>
-  construct
-    .clone()
-    .translate(
-      cardOuterGeometry.width - geometry.width,
-      cardOuterGeometry.height
-    );
+const _translate = translate(
+  cardOuterGeometry.width - geometry.width,
+  cardOuterGeometry.height
+);
+
+const receiver = path
+  .rect({
+    width: geometry.width,
+    height: geometry.height / 3,
+    radius: mm(0.5)
+  })
+  .unite(
+    path.rect({
+      width: geometry.width / 3,
+      x: geometry.width / 3,
+      y: geometry.width / 3,
+      height: geometry.height / 3
+    })
+  )
+  .unite(
+    path.rect({
+      width: geometry.width,
+      height: geometry.height / 3,
+      y: geometry.height * (2 / 3),
+      radius: mm(0.5)
+    })
+  );
 
 module.exports = {
   geometry,
-  construct: () => construct.clone(),
-  constructTranslated
+  cover: () => cover.clone(),
+  coverTranslated: () => _translate(cover.clone()),
+  receiver: () => receiver.clone(),
+  receiverTranslated: () => _translate(receiver.clone())
 };
