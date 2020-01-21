f492b8670e882a0951b0be57e178e4587d6f0df8
John Teske <john@98point6.com>
2020-01-20 17:14:18 -0800

    Create quickl release project for testing

---
+++ projects/quick-release/index.js
@@ -0,0 +1,41 @@
+const root = require("app-root-path");
+
+const path = require(`${root}/path`);
+const { cut, guide } = require(`${root}/stroke`);
+const { mm } = require(`${root}/units`);
+const { layoutRowsWithOffset } = require(`${root}/distribution`);
+
+const quickRelease = require(`${root}/constructs/button-quick-release`);
+
+const qrUnit = mm(6);
+const qr = quickRelease(qrUnit);
+
+layoutRowsWithOffset(
+  [
+    [
+      qr.outer(), qr.inner(), qr.a(),
+
+
+
+
+      qr
+        .b()
+        .translate(0, qrUnit)
+        .unite(path.rect({ width: 3 * qrUnit, height: qrUnit }))
+ 
+
+
+
+, qr.outer()
+    ].map(cut),
+    [
+      qr.guides(),
+      qr.a(),
+      qr.b(),
+     qr.inner(),
+      qr.outer()
+    ].map(guide),
+  ],
+  qrUnit
+);
+
