314c0a23ad28dc4fabd43f8b9f81866573581481
John Teske <john.teske@gmail.com>
2020-01-28 23:37:17 -0800

    Use pin sizing for quick release attachment

--- projects/palm-sandwich/constructs/quick-release.js
+++ projects/palm-sandwich/constructs/quick-release.js
@@ -3,9 +3,9 @@ const root = require("app-root-path");
 const path = require(`${root}/path`);
 const { mm } = require(`${root}/units`);
 
-const T = require("../material");
-
 const quickRelease = require(`${root}/constructs/button-quick-release`);
+
+const { pinGeometry } = require("./pin");
 
 // use a consistent size to keep connectors between iterations
 const unit = mm(6);
@@ -13,16 +13,17 @@ const qr = quickRelease(unit);
 
 const keyringAttachment = {
   x: unit * 5,
-  y: unit * 5.5
+  y: unit * 6
 };
 
-const withKeyringAttachment = o =>
+const withAttachmentPoint = o =>
   o
+    .unite(path.rect({ width: unit * 2, height: unit, x: keyringAttachment.x - unit, y: keyringAttachment.y - unit }))
     .unite(path.circle({ ...keyringAttachment, radius: unit }))
-    .subtract(path.circle({ ...keyringAttachment, radius: mm(3) / 2 }));
+    .subtract(path.circle({ ...keyringAttachment, radius: pinGeometry.r }));
 
 module.exports = {
   ...qr,
   outer: () => qr.outer(),
-  inner: () => withKeyringAttachment(qr.inner())
+  inner: () => withAttachmentPoint(qr.inner())
 };
