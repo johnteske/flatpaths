bfdc498c94f2124051f594be487298b0ca84a575
John Teske <john.teske@gmail.com>
2020-01-29 21:38:47 -0800

    Revert bolt head engrave
    This adds a lot of time to the cutting process and doesn't always
    produce usable results. With materials like acetal or wood, a
    Forstner bit could achieve the same goal.

--- projects/palm-sandwich/parts/palm-cover.js
+++ projects/palm-sandwich/parts/palm-cover.js
@@ -1,9 +1,6 @@
 const root = require("app-root-path");
 
 const path = require(`${root}/path`);
-const { cut } = require(`${root}/stroke`);
-const { engrave } = require(`${root}/fill`);
-const group = require(`${root}/group`);
 const { subtract } = require(`${root}/boolean`);
 const { pipe } = require(`${root}/fn`);
 
@@ -13,10 +10,8 @@ const frame = require("../constructs/frame");
 const {
   cardOuterGeometry,
   pins,
-  pinPoints,
   supportHoles
 } = require("../constructs/card-outer");
-const { pinGeometry } = require("../constructs/pin");
 
 const tabSize = frame.width + Math.min(palm.face.y, palm.face.y2);
 
@@ -28,14 +23,9 @@ const faceTopLeft = path.rect({
 
 const face = faceTopLeft;
 
-const part = group(
-  cut(
-    pipe(
-      ...pins().map(subtract),
-      ...supportHoles().map(subtract)
-    )(face)
-  ),
-  engrave(path.circle({ radius: pinGeometry.head.r }).translate(pinPoints[0]))
-);
+const part = pipe(
+  ...pins().map(subtract),
+  ...supportHoles().map(subtract)
+)(face);
 
 module.exports = () => part.clone();
