c99840b6e411fe4ac9fa3f83c887dfcfdb5f00f8
John Teske <john.teske@gmail.com>
2020-02-08 15:58:35 -0800

    Try round palm cover, without support pin

--- projects/palm-sandwich/parts/palm-cover.js
+++ projects/palm-sandwich/parts/palm-cover.js
@@ -2,30 +2,32 @@ const root = require("app-root-path");
 
 const path = require(`${root}/path`);
 const { subtract } = require(`${root}/boolean`);
+const { mm } = require(`${root}/units`);
 const { pipe } = require(`${root}/fn`);
 
-const palm = require(`${root}/objects/palm`);
+//const palm = require(`${root}/objects/palm`);
 
 const frame = require("../constructs/frame");
 const {
-  cardOuterGeometry,
+  //cardOuterGeometry,
   pins,
-  supportHoles
+  //supportHoles
 } = require("../constructs/card-outer");
 
-const tabSize = frame.width + Math.min(palm.face.y, palm.face.y2);
+//const tabSize = frame.width + Math.min(palm.face.y, palm.face.y2);
+const tabSize = (frame.width + mm(1)) * 2
 
 const faceTopLeft = path.rect({
   width: tabSize,
   height: tabSize,
-  radius: cardOuterGeometry.radius
+  radius: tabSize//cardOuterGeometry.radius
 });
 
 const face = faceTopLeft;
 
 const part = pipe(
   ...pins().map(subtract),
-  ...supportHoles().map(subtract)
+  //...supportHoles().map(subtract)
 )(face);
 
 module.exports = () => part.clone();
