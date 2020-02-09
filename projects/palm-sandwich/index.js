e7d6762edc173b466fd99b39c802bf2b16b0cdff
John Teske <john.teske@gmail.com>
2020-02-08 16:26:28 -0800

    Comment out QR cuts
    Ideally this will be split out into another project

--- projects/palm-sandwich/index.js
+++ projects/palm-sandwich/index.js
@@ -22,7 +22,6 @@ const palmCutoutPart = require("./parts/palm-cutout");
 const cardLayerPart = require("./parts/card-layer");
 const palmLayerWithButtonPart = require("./parts/palm-layer-with-button");
 const palmLayerPart = require("./parts/palm-layer");
-// const support = require("./parts/support");
 const palmCover = require("./parts/palm-cover");
 
 const T = require("./material");
@@ -36,7 +35,8 @@ const guides = [
     ...pins(true),
     ...supportHoles(),
     buttonTranslated(),
-    ...usbPortCutout.components()
+    ...usbPortCutout.components(),
+    palmCover()
   ),
   cardLayerPart.part(),
   palmLayerWithButtonPart.partWithReceiver(),
@@ -49,7 +49,6 @@ const acrylicCuts = [cardCoverPart(), ...cardLayerPart.components()];
 const cardboardCuts = [palmCutoutPart.part()];
 
 const cuts = [
-  // ...support(),
   ...palmLayerWithButtonPart.receiverComponents(),
   ...palmLayerWithButtonPart.components(),
   buttonPart(),
@@ -71,8 +70,8 @@ layoutRowsWithOffset(
     acrylicCuts.map(cut),
     cardboardCuts.map(cut),
     cuts.map(cut).concat(palmCover()),
-    qrCuts.map(cut),
-    beltQrCuts.map(cut),
+    //qrCuts.map(cut),
+    //beltQrCuts.map(cut),
     guides.map(guide)
   ],
   T
