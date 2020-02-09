5519e30ec902d843f1745732651d30da92d2864b
John Teske <john.teske@gmail.com>
2020-02-09 08:55:12 -0800

    Move layer marks to top of frame

--- projects/palm-sandwich/constructs/layer-mark.js
+++ projects/palm-sandwich/constructs/layer-mark.js
@@ -4,13 +4,23 @@ const paper = require("paper-jsdom");
 const frame = require("./frame");
 const group = require(`${root}/group`);
 const { score } = require(`${root}/stroke`);
-const { mm } = require(`${root}/units`);
+const { inches } = require(`${root}/units`);
 const { nItems } = require(`${root}/fn`);
+const palmCutout = require("./palm-cutout");
 
-const y = frame.width + mm(9);
-const mark = new paper.Path([[0, y], [frame.width, y]]);
+const x1 =
+  palmCutout.geometry.width / 2 + palmCutout.geometry.x - inches(1 / 16);
+const x2 = x1;
+const y1 = 0;
+const y2 = frame.width;
 
-const marks = n => nItems(n).map((_, i) => mark.clone().translate(0, i * 10));
+const dx = -1 * inches(1 / 16);
+const dy = 0;
+
+const mark = new paper.Path([[x1, y1], [x2, y2]]);
+
+const marks = n =>
+  nItems(n).map((_, i) => mark.clone().translate(i * dx, i * dy));
 
 const withMarks = (_, n) => group(_, ...marks(n).map(score));
 
