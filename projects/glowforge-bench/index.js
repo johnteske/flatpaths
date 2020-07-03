375a9b051f3aba028e935b2f64afc2dc6c7aaa54
John Teske <john.teske@protonmail.com>
2020-07-03 13:35:34 -0700

    Add basic parts

--- projects/glowforge-bench/index.js
+++ projects/glowforge-bench/index.js
@@ -8,6 +8,7 @@ const { layoutRowsWithOffset } = require(`${root}/distribution`);
 
 layoutRowsWithOffset(
   [
+    // stock
     [
       // 2x4 ft
       path.rect({
@@ -19,8 +20,26 @@ layoutRowsWithOffset(
         width: inches(24),
         height: inches(24)
       })
-    ].map(cut)
+    ].map(cut),
     //].map(guide)
+    // parts
+    [
+      // shelf sides and inner (5 each shelf unit)
+      path.rect({
+        width: inches(12),
+        height: inches(12)
+      }),
+      // shelf back (1 each shelf unit)
+      path.rect({
+        width: inches(12),
+        height: inches(24) // TODO this will be too short, given the height added by the playwood connector gaps on the sides
+      }),
+      // bench top
+      path.rect({
+        width: inches(36),
+        height: inches(24)
+      })
+    ].map(cut)
   ],
   inches(3 / 4)
 );
