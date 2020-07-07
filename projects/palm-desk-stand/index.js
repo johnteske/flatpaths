191c72f3ae133615df79de725311700928d22908
John Teske <john.teske@protonmail.com>
2020-07-07 13:44:55 -0700

    Update palm stand with desk clip idea

--- projects/palm-desk-stand/index.js
+++ projects/palm-desk-stand/index.js
@@ -1,19 +1,49 @@
 const root = require("app-root-path");
 
-const { cut, guide } = require(`${root}/stroke`);
 const { layoutRowsWithOffset } = require(`${root}/distribution`);
-
-const quickRelease = require("../palm-sandwich/constructs/quick-release");
+const path = require(`${root}/path`);
+const group = require(`${root}/group`);
+const { cut, guide } = require(`${root}/stroke`);
+const { inches, mm } = require(`${root}/units`);
 
 const T = require("./material");
-const receiver = require("./parts/receiver");
+const T2 = inches(3 / 4);
+const theta = 105; // or 150
 
-const guides = [
-  // quick release, for reference
-  quickRelease.b(),
-  quickRelease.inner()
+const phone = {
+  width: mm(100), // TODO
+  height: mm(100) // TODO
+};
+
+const _fork = path
+  .rect({
+    width: inches(4) + T2 * 3,
+    height: T2 + T2 + T2
+  })
+  .subtract(
+    path.rect({
+      width: inches(4),
+      height: T2,
+      y: T2
+    })
+  );
+const _forkTransform = _ => _.translate(0, phone.height);
+
+const _back = path.rect({
+  width: T2 * 3,
+  height: phone.height
+});
+const _backTransform = _ =>
+  _.rotate(theta - 90, [0, phone.height]).translate(inches(4), 0);
+
+const side = _forkTransform(_fork.clone()).unite(_backTransform(_back.clone()));
+
+const guides = [_fork.clone(), _back.clone()];
+
+const placed = [
+  group(_forkTransform(_fork.clone()), _backTransform(_back.clone()))
 ];
 
-const cuts = [receiver.inner(), receiver.outer()];
+const cuts = [side.clone()];
 
-layoutRowsWithOffset([cuts.map(cut), guides.map(guide)], T);
+layoutRowsWithOffset([cuts.map(cut), placed.map(guide), guides.map(guide)], T2);
