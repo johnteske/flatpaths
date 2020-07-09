03cd678b6bec4ddc104189b29a786fdfda431239
John Teske <john.teske@protonmail.com>
2020-07-09 01:13:23 -0700

    Simplify example

--- d3-gen.js
+++ d3-gen.js
@@ -4,57 +4,27 @@ const d3 = require("d3");
 const jsdom = require("jsdom");
 const JSDOM = jsdom.JSDOM;
 
-const chartWidth = 500;
-const chartHeight = 500;
-
-const arc = d3
-  .arc()
-  .outerRadius(chartWidth / 2 - 10)
-  .innerRadius(0);
-
-const colours = [
-  "#98abc5",
-  "#8a89a6",
-  "#7b6888",
-  "#6b486b",
-  "#a05d56",
-  "#d0743c",
-  "#ff8c00"
-];
-function go(
-  pieData = [12, 31],
-  outputLocation = path.join(__dirname, "./test.svg")
-) {
+function go(outputLocation = path.join(__dirname, "./test.svg")) {
   const dom = new JSDOM("");
 
-  dom.window.d3 = d3.select(dom.window.document); //get d3 into the dom
+  // get d3 into the dom
+  dom.window.d3 = d3.select(dom.window.document);
 
   const svg = dom.window.d3
     .select("body")
-    .append("div")
-    .attr("class", "container") //make a container div to ease the saving process
     .append("svg")
     .attr("xmlns", "http://www.w3.org/2000/svg")
-    .attr("width", chartWidth)
-    .attr("height", chartHeight)
-    .append("g")
-    .attr(
-      "transform",
-      "translate(" + chartWidth / 2 + "," + chartWidth / 2 + ")"
-    );
+    .append("g");
 
   svg
-    .selectAll(".arc")
-    .data(d3.pie()(pieData))
-    .enter()
-    .append("path")
-    .attr("class", "arc")
-    .attr("d", arc)
-    .attr("fill", (d, i) => colours[i])
-    .attr("stroke", "#fff");
+    .append("rect")
+    .attr("width", 99)
+    .attr("height", 99)
+    .attr("fill", "none")
+    .attr("stroke", "#000000");
 
   //using sync to keep the code simple
-  fs.writeFileSync(outputLocation, dom.window.d3.select(".container").html());
+  fs.writeFileSync(outputLocation, dom.window.d3.select("body").html());
 }
 
 go();
