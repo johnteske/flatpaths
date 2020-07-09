f73a39bcdc1d41e0339cb11b7c97d01d8175fec2
John Teske <john.teske@protonmail.com>
2020-07-09 12:05:50 -0700

    Add helper functions for attr and cut paths

--- projects/lightswitch-cover/index.js
+++ projects/lightswitch-cover/index.js
@@ -1,11 +1,33 @@
 const root = require("app-root-path");
 const { inches } = require(`${root}/units`);
 
+const attrs = attrs => selection => {
+  Object.keys(attrs).forEach(key => selection.attr(key, attrs[key]));
+  return selection;
+};
+
+const cut = attrs({ fill: "none", stroke: "#000000" });
+
+// https://www.mcmaster.com/7533K42/
+const cover = {
+  width: inches(2 + 15 / 16),
+  height: inches(4 + 11 / 16)
+};
+const toggle = {
+  width: inches(3 / 8),
+  height: inches(15 / 16)
+};
+
 module.exports = function generate(d3, svg) {
   svg
     .append("rect")
-    .attr("width", inches(4))
-    .attr("height", 199)
-    .attr("fill", "none")
-    .attr("stroke", "#000000");
+    .call(attrs(cover))
+    .call(cut);
+
+  svg
+    .append("rect")
+    .call(attrs(toggle))
+    .attr("x", (cover.width - toggle.width) / 2)
+    .attr("y", (cover.height - toggle.height) / 2)
+    .call(cut);
 };
