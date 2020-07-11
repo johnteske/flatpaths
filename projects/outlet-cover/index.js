02c4767f9421661e8817e68f46a28aa2b3da42f1
John Teske <john.teske@protonmail.com>
2020-07-10 18:27:13 -0700

    Update generate to use exported function of project

--- projects/outlet-cover/index.js
+++ projects/outlet-cover/index.js
@@ -1,12 +1,7 @@
 const root = require("app-root-path");
+const path = require(`${root}/path`);
 const { inches } = require(`${root}/units`);
-
-const attrs = attrs => selection => {
-  Object.keys(attrs).forEach(key => selection.attr(key, attrs[key]));
-  return selection;
-};
-
-const cut = attrs({ fill: "none", stroke: "#000000" });
+const { cut, guide } = require(`${root}/stroke`);
 
 // https://www.mcmaster.com/7533K82/
 const cover = {
@@ -17,34 +12,24 @@ const toggle = {
   width: inches(1 + 5 / 16),
   height: inches(2 + 5 / 8)
 };
+toggle.x = toggle.width / -2;
+toggle.y = toggle.height / -2;
+
 const hole = {
-  r: inches(3 / 16) / 2,
+  radius: inches(3 / 16) / 2,
   x: cover.width / 2,
   y: cover.height / 2,
   dy: inches(3 + 13 / 16) / 2
 };
-const holes = [[hole.x, hole.y - hole.dy], [hole.x, hole.y + hole.dy]];
 
-module.exports = function generate(d3, svg) {
-  svg
-    .append("rect")
-    .call(attrs(cover))
-    .call(cut);
-
-  svg
-    .append("rect")
-    .call(attrs(toggle))
-    .attr("x", cover.width / 2)
-    .attr("y", cover.height / 2)
-    .attr("transform", `translate(${toggle.width / -2},${toggle.height / -2})`)
-    .call(cut);
-
-  svg
-    .selectAll("circle")
-    .data(holes)
-    .enter()
-    .append("circle")
-    .attr("r", hole.r)
-    .attr("transform", d => `translate(${d[0]}, ${d[1]})`)
-    .call(cut);
+module.exports = function generate() {
+  cut(
+    path
+      .rect({ ...cover, radius: inches(1 / 16) })
+      .subtract(
+        path.rect({ ...toggle }).translate(cover.width / 2, cover.height / 2)
+      )
+      .subtract(path.circle(hole).translate(0, hole.dy))
+      .subtract(path.circle(hole).translate(0, -hole.dy))
+  );
 };
