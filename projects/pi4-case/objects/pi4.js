1919c0c55f790a76d3272858ffbf879c11b69756
John Teske <john.teske@protonmail.com>
2020-08-08 12:12:39 -0700

    Add pi4 connectors as guides for connections

--- projects/pi4-case/objects/pi4.js
+++ projects/pi4-case/objects/pi4.js
@@ -1,10 +1,17 @@
 const root = require("app-root-path");
 const { mm } = require(`${root}/units`);
 
-module.exports = {
+const pi4 = {
   width: mm(85),
   _length: mm(56),
   height: mm(16),
   radius: mm(3),
   _overhang: mm(3) // amount that some ports hang over PCB
 };
+
+pi4._connections = {
+  ethernet: [pi4.width, pi4._length - mm(45.75)],
+  power: [mm(3.5 + 7.7), pi4._length]
+};
+
+module.exports = pi4;
