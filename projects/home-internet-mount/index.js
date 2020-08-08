64573e2f2fad3df1c29861f8ecc24fe99fcccdeb
John Teske <john.teske@protonmail.com>
2020-08-08 14:29:10 -0700

    Add ethernet and power connections

--- projects/home-internet-mount/index.js
+++ projects/home-internet-mount/index.js
@@ -17,26 +17,87 @@ const board = path.rect({
   height: inches(12) // up to 18
 });
 
-const cable = new paper.Path([[0, 0], [99, 66]]);
-
-const label = (content, center) => {
+const label = (fontSize, fillColor) => (content, center) => {
   const text = new paper.PointText(center);
-  text.fontSize = inches(1);
+  text.fontSize = fontSize;
   text.justification = "center";
   text.content = content;
-  text.fillColor = "#00ffff";
-  text.translate(0, inches(0.333));
+  text.fillColor = fillColor;
+  text.translate(0, fontSize / 3);
   return text;
 };
+
+const componentLabel = label(inches(1), "#00ffff");
+
+const connectionColor = "#ff00ff";
+
+// TODO connections could have a type of x, y, label?, type? (usb, ac, etc)
+const connectionPoint = (content, point) =>
+  group(
+    path.circle({ radius: mm(3), strokeColor: connectionColor }),
+    label(inches(0.3), connectionColor)(content)
+  ).translate(point);
+
+// TODO this is linear, for now
+const connection = (p1, p2) => {
+  const line = new paper.Path.Line(p1, p2);
+  line.strokeColor = connectionColor;
+  return line;
+};
+
+// TODO
+let routerPower;
+let routerEthernetIn;
+let routerEthernet1;
+let routerEthernet2;
+let routerEthernet3;
+let routerEthernet4;
+let ac;
+let p1;
+let p2;
+let p3;
+let p4;
+let pi4Ethernet;
+let pi4Power;
 
 group(
   cut(board),
   // router
-  group(guide(router), label("router", router.bounds.center)).translate(
-    boardMargin
-  ),
+  group(
+    guide(router),
+    componentLabel("router", router.bounds.center),
+    (routerPower = connectionPoint("power", router.data.connections.power)),
+    (routerEthernetIn = connectionPoint(
+      "ethernet in",
+      router.data.connections.ethernetIn
+    )),
+    (routerEthernet1 = connectionPoint(
+      "ethernet 1",
+      router.data.connections.ethernet1
+    )),
+    (routerEthernet2 = connectionPoint(
+      "ethernet 2",
+      router.data.connections.ethernet2
+    )),
+    (routerEthernet3 = connectionPoint(
+      "ethernet 3",
+      router.data.connections.ethernet3
+    )),
+    (routerEthernet4 = connectionPoint(
+      "ethernet 4",
+      router.data.connections.ethernet4
+    ))
+  ).translate(boardMargin),
   // power strip
-  group(guide(powerStrip), label("power", powerStrip.bounds.center)).translate(
+  group(
+    guide(powerStrip),
+    componentLabel("power", powerStrip.bounds.center),
+    (ac = connectionPoint("ac", powerStrip.data.connections.ac)),
+    (p1 = connectionPoint("1", powerStrip.data.connections.plug1)),
+    (p2 = connectionPoint("2", powerStrip.data.connections.plug2)),
+    (p3 = connectionPoint("3", powerStrip.data.connections.plug3)),
+    (p4 = connectionPoint("4", powerStrip.data.connections.plug4))
+  ).translate(
     (board.bounds.width - powerStrip.bounds.width) / 2,
     board.bounds.height - boardMargin - powerStrip.bounds.height
   ),
@@ -44,17 +105,24 @@ group(
   group(
     guide(pi4Outer),
     // connections
-    group(guide(path.circle({ radius: mm(3) })), label("ethernet")).translate(
+    (pi4Ethernet = connectionPoint(
+      "ethernet",
       pi4Outer.data.connections.ethernet
-    ),
-    group(guide(path.circle({ radius: mm(3) })), label("power")).translate(
-      pi4Outer.data.connections.power
-    ),
-    label("pi4", pi4Outer.bounds.center)
+    )),
+    (pi4Power = connectionPoint("power", pi4Outer.data.connections.power)),
+    componentLabel("pi4", pi4Outer.bounds.center)
   ).translate(
     board.bounds.width - boardMargin - pi4Outer.bounds.width,
     boardMargin
   ),
-  // TODO cable management
-  guide(cable)
+  // ethernet
+  connection(routerEthernetIn.bounds.center, board.bounds.topLeft), // to modem
+  connection(routerEthernet1.bounds.center, pi4Ethernet.bounds.center),
+  connection(routerEthernet2.bounds.center, board.bounds.topCenter), // for laptop
+  connection(routerEthernet3.bounds.center, board.bounds.topCenter), // for laptop
+  // power
+  connection(routerPower.bounds.center, p1.bounds.center),
+  connection(board.bounds.bottomLeft, p3.bounds.center), // to modem
+  connection(pi4Power.bounds.center, p2.bounds.center),
+  connection(ac.bounds.center, board.bounds.bottomRight) // to outlet
 );
--- projects/home-internet-mount/power-strip.js
+++ projects/home-internet-mount/power-strip.js
@@ -3,9 +3,24 @@ const root = require("app-root-path");
 const path = require(`${root}/path`);
 const { inches } = require(`${root}/units`);
 
+const width = inches(7);
+const height = inches(1 + 3 / 8);
+
 const strip = path.rect({
-  width: inches(7),
-  height: inches(1 + 3 / 8)
+  width,
+  height
 });
+
+// TODO these are approx
+const y = height / 2;
+const connections = {
+  ac: [0, y],
+  plug1: [width - inches(4), y],
+  plug2: [width - inches(3), y],
+  plug3: [width - inches(2), y],
+  plug4: [width - inches(1), y]
+};
+
+strip.data = { connections };
 
 module.exports = () => strip.clone();
