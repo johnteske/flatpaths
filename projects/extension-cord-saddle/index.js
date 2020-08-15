c5d1fb392e2590ca69f4337d92755a049d4c50ff
John Teske <john.teske@protonmail.com>
2020-08-14 18:26:50 -0700

    Use finger joints for v2 extension cord saddle
    Heating and bending the acrylic has too large a bend radius
    and is hard to form precisely. Using finger joints and a wood
    material makes this more precise, and it can be painted to match
    the wall color.

--- projects/extension-cord-saddle/index.js
+++ projects/extension-cord-saddle/index.js
@@ -1,61 +1,85 @@
 // v1 bent acrylic (with heat gun)
+// v2 finger joints
+
 const root = require("app-root-path");
-const paper = require("paper-jsdom");
 
 const path = require(`${root}/path`);
-const { cut, score } = require(`${root}/stroke`);
+const { pipe, nItems } = require(`${root}/fn`);
+const { cut } = require(`${root}/stroke`);
 const { inches } = require(`${root}/units`);
+const { unite } = require(`${root}/boolean`);
+const { translate } = require(`${root}/transform`);
+const { layoutRowsWithOffset } = require(`${root}/distribution`);
 
 const T = inches(1 / 8);
+const N_FINGERS = 5;
 
 const anchor = {
   shaftDiameter: inches(1 / 8) // TODO measure
 };
 
-const anchorHole = path
-        .circle({ radius: anchor.shaftDiameter / 2 })
-
-const cordConnector = {
-  width: inches(1),
-  height: inches(7 / 8)
-};
+const anchorHole = path.circle({ radius: anchor.shaftDiameter / 2 });
 
 const saddleFoot = {
   width: inches(1),
   height: inches(1)
 };
 
-const segments =  [
-    saddleFoot.width,
-    T,
-    cordConnector.height,
-    T,
-    cordConnector.width,
-    T,
-    cordConnector.height,
-    T,
-    saddleFoot.width
-]
-
-const part = {
-  width: segments.reduce((len, w) => len + w, 0),
-  height: saddleFoot.height
+const finger = {
+  width: T,
+  height: saddleFoot.height / N_FINGERS
 };
+finger.part = path.rect(finger);
 
-cut(
+const withFingers = fingers => pipe(...fingers.map(s => unite(s)));
+
+const fingers = mod =>
+  nItems(N_FINGERS)
+    .map((_, i) => finger.part.clone().translate(0, finger.height * i))
+    .filter((_, i) => i % 2 === mod);
+
+saddleFoot.part = withFingers(fingers(0).map(translate(saddleFoot.width, 0)))(
   path
-    .rect(part)
+    .rect(saddleFoot)
     .subtract(
-    anchorHole.clone()
-        .translate(saddleFoot.width / 2, saddleFoot.height / 2)
-    )
-   .subtract(
-    anchorHole.clone()
-        .translate(part.width - saddleFoot.width / 2, saddleFoot.height / 2)
+      anchorHole.clone().translate(saddleFoot.width / 2, saddleFoot.height / 2)
     )
 );
 
-segments.map((w, i) => {
-  const x = segments.slice(0,i).reduce((len, w2) => len + w2, 0 )
-  return new paper.Path([[x,0],[x, saddleFoot.height]]) 
-}).map(score)
+const cordConnector = {
+  width: inches(1),
+  height: inches(7 / 8)
+};
+cordConnector.partTop = withFingers(
+  fingers(0).map(translate(cordConnector.width, 0))
+)(
+  withFingers(fingers(0).map(translate(-T, 0)))(
+    path.rect({
+      width: cordConnector.width,
+      height: saddleFoot.height
+    })
+  )
+);
+cordConnector.partSide = withFingers(
+  fingers(1).map(translate(cordConnector.height, 0))
+)(
+  withFingers(fingers(1).map(translate(-T, 0)))(
+    path.rect({
+      width: cordConnector.height,
+      height: saddleFoot.height
+    })
+  )
+);
+
+layoutRowsWithOffset(
+  [
+    [
+      saddleFoot.part.clone(),
+      cordConnector.partSide.clone(),
+      cordConnector.partTop.clone(),
+      cordConnector.partSide.clone(),
+      saddleFoot.part.clone()
+    ].map(cut)
+  ],
+  3 * T
+);
