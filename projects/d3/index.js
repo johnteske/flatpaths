87ba5ead0e5c32a9c364805c554c3f7addd39f48
John Teske <john.teske@protonmail.com>
2020-08-17 08:31:06 -0700

    Add drawer divider example, WIP

--- projects/d3/index.js
+++ projects/d3/index.js
@@ -1,7 +1,7 @@
 const fs = require("fs");
 const d3 = require("d3");
-
 const { JSDOM } = require("jsdom");
+
 const svg = JSDOM.fragment("<svg>").firstChild;
 
 const g = d3
@@ -9,12 +9,55 @@ const g = d3
   .attr("xmlns", "http://www.w3.org/2000/svg")
   .append("g");
 
-g.append("rect")
-  .attr("fill", "none")
-  .attr("stroke", "black")
-  .attr("x", 20)
-  .attr("height", 99)
-  .attr("width", 99);
+//
+
+const root = require("app-root-path");
+const { inches } = require(`${root}/units`);
+
+// function generate(g, d3)
+
+const WIDTH = inches(10);
+const DEPTH = inches(5);
+const HEIGHT = inches(3);
+
+const cut = selection => {
+  // TODO return/type?
+  selection.attr("fill", "none").attr("stroke", "black");
+};
+
+// horizontal
+const horizontal = g.append("g");
+
+horizontal
+  .append("rect")
+  .attr("width", WIDTH)
+  .attr("height", HEIGHT)
+  .call(cut);
+
+const notches = [99, 123, 500];
+
+horizontal
+  .selectAll("rect.notch")
+  .data(notches)
+  .enter()
+  .append("rect")
+  .attr("width", 9)
+  .attr("height", HEIGHT / 2)
+  .attr("x", d => d)
+  .call(cut);
+
+// vertical
+const vertical = g.append("g");
+
+vertical
+  .append("rect")
+  .attr("width", DEPTH)
+  .attr("height", HEIGHT)
+  .call(cut);
+
+vertical.attr("transform", `translate(0,${HEIGHT})`);
+
+//
 
 const out = svg.outerHTML;
 
