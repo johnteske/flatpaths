8fe45dd2e1d589d5dd9bf452aa4655f336512dce
John Teske <john.teske@protonmail.com>
2020-08-19 09:03:49 -0700

    Sketch out laser cutter bench, v2

--- projects/glowforge-bench/index.js
+++ projects/glowforge-bench/index.js
@@ -1,77 +1,47 @@
+// v1 two side shelves, with top
+// - two shelves for materials makes up the full bench width, so they should just be one unit
+// - joining shelves in the middle to top cannot be done with the connectors (and mid support is needed)
+// v2
+// - one large shelf with divided shelves on bottom and a mid support on top
+
 // TODO a reminder that nominal size !== actual size and that with kerf, parts will need to be smaller than the actual stock size
+
 const paper = require("paper-jsdom");
 const root = require("app-root-path");
 
 const path = require(`${root}/path`);
 const { cut, guide } = require(`${root}/stroke`);
-//const { fillColor } = require(`${root}/fill`);
 const group = require(`${root}/group`);
-const { translate } = require(`${root}/transform`);
 const { inches } = require(`${root}/units`);
 const { layoutRowsWithOffset } = require(`${root}/distribution`);
 
 // ignoring connector gaps
 const dimensions = {
   width: inches(36),
-  depth: inches(20.75), // 20.75-24 in
+  depth: inches(24), // inches(20.75), // 20.75-24 in
   height: inches(36)
 };
 
 const kerf = inches(1 / 2); // TODO need to calculate
-const sawCut = length =>
-  path.rect({
-    height: kerf,
-    width: length
-  });
 
-const benchTop = {
+const largePanel = {
   width: dimensions.width,
   height: dimensions.depth
 };
-benchTop.part = path.rect({
-  width: benchTop.width,
-  height: benchTop.height
-});
+largePanel.part = path.rect(largePanel);
 
-// need 4 per shelf (each side has top and bottom)
-const shelfSide = {
+const smallPanel = {
   width: dimensions.depth,
   height: dimensions.height / 2
 };
-shelfSide.part = path.rect({
-  // dimensions are flipped to help understand visually
-  width: shelfSide.height,
-  height: shelfSide.width
-});
+smallPanel.part = path.rect(smallPanel);
 
-// 2 per shelf (each back has top and bottom)
-const shelfBack = {
-  width: inches(12),
-  height: dimensions.height / 2
+// TODO get exact dimensions
+const plywood = {
+  width: inches(8 * 12),
+  height: inches(4 * 12)
 };
-shelfBack.part = path.rect({
-  width: shelfBack.width,
-  height: shelfBack.height
-});
-
-// one per shelf
-const shelfInner = {
-  width: shelfBack.width,
-  height: dimensions.depth
-};
-shelfInner.part = path.rect({
-  width: shelfInner.width,
-  height: shelfInner.height
-});
-
-const ply2x4 = {
-  width: inches(23.8125),
-  height: inches(47.8125),
-  part: path.rect({
-    width: inches(23.8125),
-    height: inches(47.8125)
-  })
-};
+plywood.part = path.rect(plywood);
 
 const fontSize = 96 * 2;
 
@@ -91,27 +61,31 @@ layoutRowsWithOffset(
   [
     // stock
     [
-      // 2x4 ft
-      // https://www.lowes.com/pd/3-4-in-Common-Fir-Sanded-Plywood-Application-as-2-x-4/1000068963
       group(
-        ply2x4.part,
+        plywood.part,
         // parts to cut
-        benchTop.part
-          .clone()
-          .rotate(90, [0, 0])
-          .translate(benchTop.height, 0),
-        translate(0, benchTop.width)(sawCut(ply2x4.width))
+        // TODO this is a manual approach, not optimized
+        ...layoutRowsWithOffset(
+          [[largePanel.part.clone(), largePanel.part.clone()]],
+          kerf
+        ).flat(),
+        largePanel.part.clone()
       )
     ].map(guide),
     // parts
     [
-      partGroup(shelfInner.part, "inner"),
-      partGroup(shelfBack.part, "back"),
-      partGroup(shelfSide.part, "side"),
-      partGroup(benchTop.part, "top")
+      partGroup(smallPanel.part, "(4) side"),
+      partGroup(smallPanel.part, "(2) top shelf"),
+      partGroup(smallPanel.part, "(1) top mid support"),
+      partGroup(smallPanel.part, "(1) btm mid support"),
+      partGroup(smallPanel.part, "(2) btm back")
+    ],
+    [
+      partGroup(largePanel.part, "(1) bench top"),
+      partGroup(largePanel.part, "(1) top back")
     ]
   ],
-  inches(1)
+  inches(2)
 );
 
-paper.view.viewSize = [inches(72), inches(72)];
+paper.view.viewSize = [inches(48), inches(48)];
