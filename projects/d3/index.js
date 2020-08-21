e11479de6a97815f6fc98fc8e926061c3a97f116
John Teske <john.teske@protonmail.com>
2020-08-20 18:48:19 -0700

    Laser cutter bench update

--- projects/d3/index.js
+++ projects/d3/index.js
@@ -1,13 +1,13 @@
 const root = require("app-root-path");
-const { inches } = require(`${root}/units`);
+const { dpi, inches } = require(`${root}/units`);
 const { nItems } = require(`${root}/fn`);
 
 module.exports = function generate(g) {
   const T = inches(1 / 8);
 
-  const WIDTH = inches(10);
-  const DEPTH = inches(8);
-  const HEIGHT = inches(3);
+  const WIDTH = inches(8);
+  const DEPTH = inches(12);
+  const HEIGHT = inches(3.5);
 
   const SLOT_SPACING = inches(2.5);
 
@@ -15,6 +15,10 @@ module.exports = function generate(g) {
     // TODO return/type for `call`ed functions?
     selection.attr("fill", "none").attr("stroke", "black");
   };
+
+  //  const score = selection => {
+  //    selection.attr("fill", "none").attr("stroke", "magenta");
+  //  }
 
   const notchPositions = width =>
     nItems(Math.floor((width - T * 2) / SLOT_SPACING) + 1).map(
@@ -39,10 +43,22 @@ module.exports = function generate(g) {
   horizontal
     .append("rect")
     .attr("width", WIDTH)
-    .attr("height", HEIGHT)
-    .call(cut);
+    .attr("height", HEIGHT);
 
   horizontal.call(makeNotches(notchPositions(WIDTH)));
+
+  horizontal
+    .append("text")
+    .text(`${WIDTH / dpi} x ${HEIGHT / dpi}″; ${SLOT_SPACING / dpi}″ slots`)
+    .attr("font-family", "monospace")
+    .attr("font-size", T)
+    .attr("fill", "magenta")
+    .attr("stroke", "none")
+    .attr("x", T)
+    .attr("y", HEIGHT / 2)
+    .attr("dy", "2em");
+
+  horizontal.call(cut);
 
   // vertical
   const vertical = g.append("g");
@@ -50,10 +66,9 @@ module.exports = function generate(g) {
   vertical
     .append("rect")
     .attr("width", DEPTH)
-    .attr("height", HEIGHT)
-    .call(cut);
+    .attr("height", HEIGHT);
 
   vertical.call(makeNotches(notchPositions(DEPTH)));
 
-  vertical.attr("transform", `translate(0,${HEIGHT})`);
+  vertical.attr("transform", `translate(0,${HEIGHT + T})`).call(cut);
 };
