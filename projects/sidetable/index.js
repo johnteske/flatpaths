9e67d5ba55a5fad5e0c3c53241f756b187660edf
John Teske <john.teske@protonmail.com>
2021-02-07 17:27:29 -0800

    Add example object, for reference

--- projects/sidetable/index.js
+++ projects/sidetable/index.js
@@ -1,6 +1,6 @@
 const root = require("app-root-path");
 const { inches } = require(`${root}/units`);
-const { cut, guide } = require(`${root}/d3/stroke`);
+const { stroke, guide } = require(`${root}/d3/stroke`);
 
 const T = inches(3 / 4);
 const T2 = inches(1 / 4);
@@ -12,6 +12,27 @@ const HEIGHT = inches(18);
 const SIDE_HEIGHT = HEIGHT - 2 * T;
 const SHELF_WIDTH = (WIDTH - 3 * T) / 2;
 const INNER_DEPTH = DEPTH - 99; // TODO how much space is needed?
+
+//const label = selection =>
+//  selection.attr("fill", "magenta").attr("stroke", "none");
+//
+//const centerLabel = (x, y) => selection =>
+//  selection
+//    .attr("text-anchor", "middle")
+//    .attr("x", x / 2)
+//    .attr("y", y / 2);
+
+const objectReference = stroke("magenta");
+
+const PS4 = {};
+const PS5 = {};
+const SWITCH = {};
+const LAPTOP = {
+  width: inches(13.2),
+  depth: inches(9.4),
+  height: inches(1.3)
+};
+const SCREEN = {};
 
 module.exports = function generate(d3, g) {
   const panels = g.append("g").call(guide);
@@ -59,4 +80,12 @@ module.exports = function generate(d3, g) {
     .attr("y", T);
 
   shelfSpace.clone().attr("x", SHELF_WIDTH + 2 * T);
+
+  // laptop goes in side-first
+  layout
+    .append("rect")
+    .attr("width", LAPTOP.depth)
+    .attr("height", LAPTOP.height)
+    .call(objectReference)
+    .attr("transform", `translate(${T}, ${T + SIDE_HEIGHT - LAPTOP.height})`);
 };
